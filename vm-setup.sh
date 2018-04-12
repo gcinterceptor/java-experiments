c77ac9eb90e45c0391238e3ef753b6b13f800d31
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2018-04-12 09:11:49 -0300

    Add virtual-machine setup script.

---
+++ vm-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This script is a customization Script and it is used to set instances launched using openstack. 
+
+# update things.
+sudo su
+apt-get -y update && apt-get upgrade -y
+
+# install maven, pidstat and htop. 
+apt install maven -y
+apt install sysstat -y
+apt install htop -y
+
+# install wrk2.
+apt-get install build-essential libssl-dev git -y
+git clone https://github.com/giltene/wrk2.git
+cd wrk2
+make
+cp wrk /usr/local/bin
+cd ../
+
+# install openjdk.
+apt-get install openjdk-8-jdk -y
+apt-get -o Dpkg::Options::="--force-overwrite" install openjdk-9-jdk -y
+
+# install nginx. Remove the hash character below to do it if needed.
+# apt-get install nginx -y
+
+
