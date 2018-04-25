9262bc048d39783f7da8f45b1d2c942360fccd57
Daniel Fireman <danielfireman@gmail.com>
2018-04-25 17:02:07 -0300

    Using datapackages to deal with accesslogs. Creating go script to deal process accesslogs and generate simulator input file

---
+++ go/src/altosim/main.go
@@ -0,0 +1,51 @@
+// Package to process accesslogs and produce a CSV that will be consumed by gci-simulator.
+//
+// Usage example:
+// ./altosim --al_datapackage=../../../2i/al_datackage.json --resource=gci --warmup=120s > sim_gci.csv
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/frictionlessdata/datapackage-go/datapackage"
+	"github.com/frictionlessdata/tableschema-go/csv"
+)
+
+var (
+	alPkg    = flag.String("al_datapackage", "", "Path to access logs.")
+	warmup   = flag.Duration("warmup", 120*time.Second, "Duration of the warmup time")
+	resource = flag.String("resource", "gci", "Resource name (e.g. gci, nogci")
+)
+
+type entry struct {
+	Timestamp   float64 `tableheader:"timestamp"`
+	Status      int32   `tableheader:"status"`
+	RequestTime float64 `tableheader:"request_time"`
+}
+
+func main() {
+	flag.Parse()
+	al, err := datapackage.Load(*alPkg)
+	if err != nil {
+		panic(err)
+	}
+
+	var entries []entry
+	gci := al.GetResource(*resource)
+	if err := gci.Cast(&entries, csv.Delimiter(';'), csv.LoadHeaders()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("timestamp,status,request_time") // Output header.
+
+	first := entries[0].Timestamp * 1000 // Converting to ms.
+	delta := float64(*warmup / 1e6)      // Converting to ms.
+	for _, e := range entries {
+		e.Timestamp = e.Timestamp * 1000
+		if e.Timestamp >= first+delta {
+			fmt.Printf("%d,%d,%d\n", int64(e.Timestamp), e.Status, int64(e.RequestTime*1000)) // Converting request time to ms.
+		}
+	}
+}
