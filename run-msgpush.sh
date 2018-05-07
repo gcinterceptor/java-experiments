af45b0f3c09ba7efbcbb6784920b833d471cf099
Daniel Fireman <danielfireman@gmail.com>
2018-05-07 18:45:39 -0300

    Remove flag --latency from wrk invocation.

--- run-msgpush.sh
+++ run-msgpush.sh
@@ -21,7 +21,7 @@ then
 else
     USE_GCI="true"
     FILE_NAME_SUFFIX="gci"
-    START_PROXY="killall gci-proxy 2>/dev/null; nohup ./gci-proxy >unavailability.csv 2>/dev/null & sleep 10s;"
+    START_PROXY="killall gci-proxy 2>/dev/null; rm proxy_latency.csv 2>/dev/null; GOMAXPROCS=1 nohup ./gci-proxy >unavailability.csv 2>/dev/null & sleep 5s;"
     SERVER_PORT=8080
 fi
 
@@ -53,17 +53,17 @@ do
     for instance in ${INSTANCES};
     do
         ssh ${instance} "${START_PROXY} killall java 2>/dev/null; rm gc.log shed.csv st.csv 2>/dev/null; killall pidstat 2>/dev/null; USE_GCI=${USE_GCI} PORT=${SERVER_PORT} SHED_RATIO_CSV_FILE=shed.csv WINDOW_SIZE=${WINDOW_SIZE} MSG_SIZE=${MSG_SIZE} COMPUTING_TIME_MS=${COMPUTING_TIME_MS} SLEEP_TIME_MS=${SLEEP_TIME_MS} nohup java ${JVMARGS} -jar  msgpush.jar >msgpush.out 2>msgpush.err & nohup pidstat -C java 1 | grep java | sed s/,/./g |  awk '{if (\$0 ~ /[0-9]/) { print \$1\",\"\$2\",\"\$3\",\"\$4\",\"\$5\",\"\$6\",\"\$7\",\"\$8\",\"\$9; }  }'> cpu.csv 2>/dev/null &"
-    done
+    done 
 
     sleep 5
     echo "round ${round}: Done. Starting load test..."
-    ssh ${LB} "sudo rm /var/log/nginx/*.log;  sudo systemctl restart nginx; killall wrk 2>/dev/null; ${WRK} -t${THREADS} -c${CONNECTIONS} -d${EXPERIMENT_DURATION} -R${THROUGHPUT} --latency --timeout=15s http://localhost > ~/wrk_${FILE_NAME_SUFFIX}_${round}.out; cp /var/log/nginx/access.log ~/al_${FILE_NAME_SUFFIX}_${round}.log; cp /var/log/nginx/error.log ~/nginx_error_${FILE_NAME_SUFFIX}_${round}.log"
+    ssh ${LB} "sudo rm /var/log/nginx/*.log;  sudo systemctl restart nginx; killall wrk 2>/dev/null; ${WRK} -t${THREADS} -c${CONNECTIONS} -d${EXPERIMENT_DURATION} -R${THROUGHPUT} --timeout=1s http://localhost > ~/wrk_${FILE_NAME_SUFFIX}_${round}.out; cp /var/log/nginx/access.log ~/al_${FILE_NAME_SUFFIX}_${round}.log; cp /var/log/nginx/error.log ~/nginx_error_${FILE_NAME_SUFFIX}_${round}.log"
 
     echo "round ${round}: Done. Putting server instances down..."
     i=0
     for instance in ${INSTANCES};
     do
-        cmd="killall java; killall gci-proxy; killall pidstat; mv cpu.csv cpu_${FILE_NAME_SUFFIX}_${i}_${round}.csv; mv gc.log gc_${FILE_NAME_SUFFIX}_${i}_${round}.log; mv shed.csv shed_${FILE_NAME_SUFFIX}_${i}_${round}.csv; mv st.csv st_${FILE_NAME_SUFFIX}_${i}_${round}.csv"
+        cmd="killall java; killall gci-proxy; killall pidstat; mv cpu.csv cpu_${FILE_NAME_SUFFIX}_${i}_${round}.csv; mv gc.log gc_${FILE_NAME_SUFFIX}_${i}_${round}.log; mv shed.csv shed_${FILE_NAME_SUFFIX}_${i}_${round}.csv; mv st.csv st_${FILE_NAME_SUFFIX}_${i}_${round}.csv; mv proxy_latency.csv proxy_latency_${FILE_NAME_SUFFIX}_${i}_${round}.csv"
         ssh ${instance} "$cmd"
         ((i++))
     done
@@ -76,9 +76,9 @@ do
     i=0
     for instance in ${INSTANCES};
     do
-        scp ${instance}:~/\{cpu*.csv,gc*.log,shed*.csv,st*.csv\} ${OUTPUT_DIR}
+        scp ${instance}:~/\{cpu*.csv,gc*.log,shed*.csv,st*.csv,proxy_latency*.csv\} ${OUTPUT_DIR}
         sed -i '1i time,ampm,uid,pid,usr,system,guest,cpu,cpuid' ${OUTPUT_DIR}/cpu_${FILE_NAME_SUFFIX}_${i}_${round}.csv
-        ssh ${instance} "rm ~/cpu*.csv ~/gc*.log ~/shed*.csv ~/st*.csv"
+        ssh ${instance} "rm ~/cpu*.csv ~/gc*.log ~/shed*.csv ~/st*.csv ~/proxy_latency*.csv"
         ((i++))
     done
     echo "round ${round}: Finished."
